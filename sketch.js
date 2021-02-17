750d3e5a73515c8a65f339635d6e9918e7e8edcd
priyapandey2020 <66728335+priyapandey2020@users.noreply.github.com>
2021-02-17 13:19:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,30 @@
-var box1, box2;
+var fixedRect, movingRect;
 
 function setup() {
-  createCanvas(400,400);
-  box1 = createSprite(200, 200, 50, 80);
-  box1.shapeColor = "green";
-  box1.debug = true;
-  box2 = createSprite(200,200,80,30);
-  box2.shapeColor = "green";
-  box2.debug = true;
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 }
 
 function draw() {
   background(0,0,0);  
-  box2.x = World.mouseX;
-  box2.y = World.mouseY;
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
 
-  if (box2.x - box1.x === box1.width/2 + box2.width/2) {
-    box2.shapeColor = "red";
-    box1.shapeColor = "red";
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
   }
   else {
-    box2.shapeColor = "green";
-    box1.shapeColor = "green";
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
   }
   drawSprites();
 }
